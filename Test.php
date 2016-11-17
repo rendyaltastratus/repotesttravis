b1d7187769b6bff0fc80ea905661d0281730ccb2
Rendy AltastratUS <rendy@altastrat.us>
2016-11-17 15:19:26 +0700

    test travis

--- Test.php
+++ Test.php
@@ -1,3 +1,17 @@
 <?php
-echo false;
+function inverse($x) {
+    if (!$x) {
+        throw new Exception('Division by zero.');
+    }
+    return 1/$x;
+}
+
+try{
+  echo inverse(0);
+}
+catch(Exception $e ){
+  echo 'Caught exception: ',  $e->getMessage(), "\n";
+
+}
+
 ?>
