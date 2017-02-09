e9c2f25249f51c8c43847dce8dd222cd16f701e5
Rendy AltastratUS <rendy@altastrat.us>
2017-02-09 15:53:29 +0700

    Test ke 19

--- Test.php
+++ Test.php
@@ -1,45 +1,74 @@
 <?php
-// set_error_handler('myErrorHandler');
-// register_shutdown_function('fatalErrorShutdownHandler');
-// function myErrorHandler($code, $message, $file, $line) {
-//   var_dump($code,$message,$file,$line);
-// }
-// function fatalErrorShutdownHandler()
-// {
-//   $last_error = error_get_last();
-//   if ($last_error['type'] === E_ERROR) {
-//     // fatal error
-//     myErrorHandler(E_ERROR, $last_error['message'], $last_error['file'], $last_error['line']);
-//   }
-// }
-// The key is that functions registered with register_shutdown_function()
-// are called even on a fatal error - including out of memory errors.
-// error_get_last() can then be used to detect whether we’re ending the script because of a fatal error, and pass the error info to your custom error handler if so.
-function inverse($x) {
-    if (!$x) {
-        throw new Exception('Division by zero.');
+ini_set('display_errors',1);
+class TravisControllerCI{
+  var $filename= 'travis.ci.txt';
+  var $domain="domain.com";
+  var $ip_address="";
+  var $result;
+
+  public function __construct($filename="",$domain="",$ip_address=""){
+      $this->filename=$filename==""?$this->filename:$filename;
+      $this->domain=$domain;
+      $this->ip_address=$ip_address;
+      $this->travis_curl_get();
+  }
+
+  function travis_curl_get(){
+
+      $ch = curl_init($this->ip_address);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array('Host: '.($this->domain)));
+      curl_setopt($ch, CURLOPT_URL, "http://".($this->ip_address)."/".($this->filename));
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      $data= curl_exec($ch);
+      curl_close($ch);
+      $this->result=$data;
+      return $data;
+  }
+
+  function result_(){
+    return $this->result;
+      // trigger_error($this->result, E_USER_ERROR);
+  }
+
+  function run(){
+
+      if(strpos($this->result,'error')!==false){
+        $data="Travis not allowed to Commit or somethings, please check your server Error first ";
+        trigger_error($data, E_USER_ERROR);
     }
-    return 1/$x;
+  }
+
 }
 
-try{
-  echo inverse(0);
-}
-catch(Exception $e ){
-  ini_set('display_errors',1);
+$TravisControllerCI = new TravisControllerCI("","devaudi.org","199.223.211.102");
 
-  //var_dump("test");
-  // echo 'Caught exception: ',  $e->getMessage(), "\n";
+$TravisControllerCI->run();
 
-    $ch = curl_init('199.223.211.102');
-     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Host: devaudi.org'));
-    curl_setopt($ch, CURLOPT_URL, $url="http://199.223.211.102/COPYRIGHT.txt");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $data= curl_exec($ch);
-    curl_close();
-    var_dump($data);
-
-    trigger_error(print_r($c,1).": Travis need More", E_USER_ERROR);
-}
+// function inverse($x) {
+//     if (!$x) {
+//         throw new Exception('Division by zero.');
+//     }
+//     return 1/$x;
+// }
+//
+// try{
+//   echo inverse(0);
+// }
+// catch(Exception $e ){
+//   ini_set('display_errors',1);
+//
+//   //var_dump("test");
+//   // echo 'Caught exception: ',  $e->getMessage(), "\n";
+//
+//     $ch = curl_init('199.223.211.102');
+//     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Host: devaudi.org'));
+//     curl_setopt($ch, CURLOPT_URL, $url="http://199.223.211.102/COPYRIGHT.txt");
+//     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//     $data= curl_exec($ch);
+//     curl_close();
+//     var_dump($data.=": Travis need More");
+//
+//     trigger_error($data, E_USER_ERROR);
+// }
 
 ?>
